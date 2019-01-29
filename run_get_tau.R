7053ea25b713afe727602b14c15c341c357af39d
Tim Pollington <timothy.pollington@gmail.com>
2019-01-29 18:32:23 +0000

    Add files via upload

---
+++ run_get_tau.R
@@ -0,0 +1,10 @@
+rm(list = ls()) #clean up
+library(Rcpp) #sourceCpp()
+setwd(FolderWhereYouStoreYourData)
+load(file = "YourData.RData") #say you have called your matrix object "X"
+
+sourceCpp("get_tau.cpp")
+
+r.max = seq(50,500,50) #upper bound of each bin, in metres
+r.min = rep.int(0,length(r.max)) #lower bound of each bin
+getTau(X[,"ORIG_IDenum"],X[,"x"],X[,"y"],X[,"KA"],r.max,r.min,as.integer(-1)) #note that as we want the point estimate of tau we set the last argument to -1. You could adapt this function for bootstrapping by parsing in a vector of indices for the last argument. 
