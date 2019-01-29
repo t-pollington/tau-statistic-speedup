22cb16b8be9dcc3b21fd4016082f3777ff6f5915
Tim Pollington <timothy.pollington@gmail.com>
2019-01-29 19:10:29 +0000

    Update run_get_tau.R

--- run_get_tau.R
+++ run_get_tau.R
@@ -7,4 +7,4 @@ sourceCpp("get_tau.cpp")
 
 r.max = seq(50,500,50) #upper bound of each bin, in metres
 r.min = rep.int(0,length(r.max)) #lower bound of each bin
-getTau(X[,"ORIG_ID"],X[,"x"],X[,"y"],X[,"onset"],r.max,r.min,as.integer(-1)) #note that as we want the point estimate of tau we set the last argument to -1. You could adapt this function for bootstrapping by parsing in a vector of indices for the last argument. 
+getTau(X[,"ORIG_ID"],X[,"x"],X[,"y"],X[,"onset"],r.max,r.min,as.integer(-1)) #note that as we want the point estimate of tau we set the last argument to -1. You could adapt this function for bootstrapping by parsing in a vector of indices for the last argument.
