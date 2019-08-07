26d19cde071c4ff491bb6c954f2153b0ac68565a
t-pollington <timothy.pollington@gmail.com>
2019-08-07 14:09:01 +0100

    back to fast 1.4secs

--- anRpackage/src/get_tau.cpp
+++ anRpackage/src/get_tau.cpp
@@ -1,6 +1,6 @@
 //Credit: Inspired and amended from https://github.com/HopkinsIDD/IDSpatialStats/commit/2782d6dcc9ee4be9855b5e468ce789425b81d49a by @gilesjohnr @jlessler
 //Author: @t-pollington
-//Date: 29 Jan 2019
+//Date: 6 Aug 2019
 #include <cstdlib>
 #include <iostream>
 #include <Rcpp.h>
@@ -27,49 +27,49 @@ NumericVector getTau(const NumericVector ORIG_ID, const NumericVector x, const N
   bool iscasepair = 0;
   bool temprelated = 0;
   bool withindist = 0;
-
-if(*inds==-1){ //if index was set to -1 then it means we can turn off bootstrapping checks 
-  check = 0; 
-}
-
-num_cnt = 0;
-denom_cnt = 0;
-
-//calc piInf
-for (i=0;i<N;i++) {
-  for (j=0; j<i;j++) {
-    bstrapconflict = (inds[i] == inds[j]) && check; //do not compare someone with themself if bootstrapping*/
-    sameperson = (ORIG_ID[i]==ORIG_ID[j]); //ie the person migrated to a different place in the study
-    denom_cnt = denom_cnt + (!(bstrapconflict)*!(sameperson));
-    iscasepair = (onset[i]!=-999) && (onset[j]!=-999);
-    temprelated = (abs(onset[i]-onset[j]) <= serialintvl); //could add temporal restrictions here that the pair be within the start/end dates of the study
-    num_cnt = num_cnt + (!(bstrapconflict)*!(sameperson)*iscasepair*temprelated);
+  
+  if(*inds==-1){ //if index was set to -1 then it means we can turn off bootstrapping checks 
+    check = 0; 
   }
-}
-piInf = (double)num_cnt/denom_cnt;
-
-//calc pi(r2_low,r2)
-for (k=0;k<r_size;k++) {
+  
   num_cnt = 0;
   denom_cnt = 0;
-  r2  = pow(r[k],2); //transformation to squared distances to make sqrt() redundant in this line and the one below
-  r2_low = pow(r_low[k],2); 
-
+  
+  //calc piInf
   for (i=0;i<N;i++) {
-    for (j=0; j<i;j++) { //lower triangular access only as undirected pairs assumed
-      bstrapconflict = (inds[i] == inds[j]) && check; //do not compare someone with themself if bootstrapping
-      sameperson = (ORIG_ID[i]==ORIG_ID[j]);
-      dist2 = pow(x[i] - x[j],2) + pow(y[i] - y[j],2); //calculate the distance
-      withindist = ((dist2 >= r2_low) && (dist2 < r2));
-      if(bstrapconflict||!withindist||sameperson) continue;
-      denom_cnt = denom_cnt + 1;
+    for (j=0; j<i;j++) {
+      bstrapconflict = (inds[i] == inds[j]) && check; //do not compare someone with themself if bootstrapping*/
+      sameperson = (ORIG_ID[i]==ORIG_ID[j]); //ie the person migrated to a different place in the study
+      denom_cnt = denom_cnt + (!(bstrapconflict)*!(sameperson));
       iscasepair = (onset[i]!=-999) && (onset[j]!=-999);
-      temprelated = (abs(onset[i]-onset[j]) <= serialintvl);
-      num_cnt = num_cnt + (iscasepair*temprelated);
+      temprelated = (abs(onset[i]-onset[j]) <= serialintvl); //could add temporal restrictions here that the pair be within the start/end dates of the study
+      num_cnt = num_cnt + (!(bstrapconflict)*!(sameperson)*iscasepair*temprelated);
     }
   }
-  tau[k] = (double)num_cnt/denom_cnt; // pi(r.min,r.max)
-  tau[k] = (double)tau[k]/piInf;
-}
-return(tau);
+  piInf = (double)num_cnt/denom_cnt;
+  
+  //calc pi(r2_low,r2)
+  for (k=0;k<r_size;k++) {
+    num_cnt = 0;
+    denom_cnt = 0;
+    r2  = pow(r[k],2); //transformation to squared distances to make sqrt() redundant in this line and the one below
+    r2_low = pow(r_low[k],2);
+    
+    for (i=0;i<N;i++) {
+      for (j=0; j<i;j++) { //lower triangular access only as undirected pairs assumed
+        withindist = 0;
+        bstrapconflict = (inds[i] == inds[j]) && check; //do not compare someone with themself if bootstrapping
+        dist2 = pow(x[i] - x[j],2) + pow(y[i] - y[j],2); //calculate the distance
+        withindist = ((dist2 >= r2_low) && (dist2 < r2));
+        sameperson = (ORIG_ID[i]==ORIG_ID[j]);
+        denom_cnt = denom_cnt + (!(bstrapconflict)*!(sameperson)*withindist);
+        iscasepair = (onset[i]!=-999) && (onset[j]!=-999);
+        temprelated = (abs(onset[i]-onset[j]) <= serialintvl);
+        num_cnt = num_cnt + (!(bstrapconflict)*!(sameperson)*iscasepair*temprelated*withindist);
+      }
+    }
+    tau[k] = (double)num_cnt/denom_cnt; // pi(r.min,r.max)
+    tau[k] = (double)tau[k]/piInf;
+  }
+  return(tau);
 }
