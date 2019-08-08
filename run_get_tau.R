75d9f5da232ab6e33e501e3f09fca81655bdca52
t-pollington <timothy.pollington@gmail.com>
2019-08-08 15:25:38 +0100

    update with build instructions

--- run_get_tau.R
+++ run_get_tau.R
@@ -2,14 +2,14 @@ rm(list = ls()) #clean up from before
 library(Rcpp) #sourceCpp()
 setwd(FolderWhereYouStoreYourData)
 load(file = "YourData.RData") #say you have called your matrix object "X"
-setwd(FolderWhereYouStoreYourTauCFile)
-sourceCpp("get_tau.cpp")
+setwd("/taustatisticspeedup")
+#'Install and restart'
 
 r.max = seq(50,500,50) #upper bound of each bin, in metres
 r.min = rep.int(0,length(r.max)) #lower bound of each bin
 ptm <- proc.time()
 for (i in 1:10) {
-  getTau(X.NW[,"ORIG_IDenum"],X.NW[,"x"],X.NW[,"y"],X.NW[,"KA"],r.max,r.min,as.integer(-1)) #note that as we want the point estimate of tau we set the last argument to -1. You could adapt this function for bootstrapping by parsing in a vector of indices for the last argument.
+  taustatisticspeedup::getTau(X.NW[,"ORIG_IDenum"],X.NW[,"x"],X.NW[,"y"],X.NW[,"KA"],r.max,r.min,as.integer(-1)) #note that as we want the point estimate of tau we set the last argument to -1. You could adapt this function for bootstrapping by parsing in a vector of indices for the last argument.
 }
 timing = (proc.time() - ptm)
 timing/10 #2.0578s for commit 5dceac9 get_tau.cpp
