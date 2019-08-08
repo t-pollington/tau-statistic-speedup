49e67841f01f453e94b9d7039d9ae48bba9f1e98
t-pollington <timothy.pollington@gmail.com>
2019-08-08 21:36:46 +0100

    add getTau3

--- run_get_tau.R
+++ run_get_tau.R
@@ -9,8 +9,15 @@ r.max = seq(50,500,50) #upper bound of each bin, in metres
 r.min = rep.int(0,length(r.max)) #lower bound of each bin
 ptm <- proc.time()
 for (i in 1:10) {
-  taustatisticspeedup::getTau(X.NW[,"ORIG_IDenum"],X.NW[,"x"],X.NW[,"y"],X.NW[,"KA"],r.max,r.min,as.integer(-1)) #note that as we want the point estimate of tau we set the last argument to -1. You could adapt this function for bootstrapping by parsing in a vector of indices for the last argument.
+  taustatisticspeedup::getTau2(X.NW[,"ORIG_IDenum"],X.NW[,"x"],X.NW[,"y"],X.NW[,"KA"],r.max,r.min,as.integer(-1)) #note that as we want the point estimate of tau we set the last argument to -1. You could adapt this function for bootstrapping by parsing in a vector of indices for the last argument.
 }
 timing = (proc.time() - ptm)
 timing/10 #2.0578s for commit 5dceac9 get_tau.cpp
-#2.03s for NTYPE change to unsigned short
+r.max = seq(50,500,50) #upper bound of each bin, in metres
+r.min = rep.int(0,length(r.max)) #lower bound of each bin
+ptm <- proc.time()
+for (i in 1:10) {
+  taustatisticspeedup::getTau3(X.NW[,"ORIG_IDenum"],X.NW[,"x"],X.NW[,"y"],X.NW[,"KA"],r.max,r.min,as.integer(-1)) #note that as we want the point estimate of tau we set the last argument to -1. You could adapt this function for bootstrapping by parsing in a vector of indices for the last argument.
+}
+timing = (proc.time() - ptm)
+timing/10 #2.0578s for commit 5dceac9 get_tau.cpp
